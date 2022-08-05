51008bf7bc3d48d4fc7438bd01f34f7a9c6aa130
Echglene Woy <echglenew@gmail.com>
2022-08-05 17:46:06 -0600

    some file cleanup and added a little bit of bill component

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,3 +1,12 @@
-class Card extends React.Component {
+import React from 'react';
 
+class Card extends React.Component {
+    render() {
+        return (
+            <div>The Bill</div>
+        ); 
+        
+    }
 }
+
+export default Card;
