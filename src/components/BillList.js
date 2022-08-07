880b910eedad0dfbdd32c9b2ff68c3df090a3dd7
Echglene Woy <echglenew@gmail.com>
2022-08-07 16:50:37 -0600

    added functionality to add new bill, outputs to console

--- src/components/BillList.js
+++ src/components/BillList.js
@@ -1,12 +1,45 @@
 import React, { Component } from 'react';
 
 class BillList extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            items: []
+        };
+
+        this.addBill = this.addBill.bind(this);
+    }    
+
+    addBill(e) {
+        if (this._inputElement.value !== "") {
+            var newItem = {
+                text: this._inputElement.value,
+                key: Date.now()
+            };
+
+            this.setState((prevState) => {
+                return {
+                    items: prevState.items.concat(newItem)
+                };
+            });
+
+        this._inputElement.value = "";
+        }
+
+        console.log(this.state.items);
+
+        e.preventDefault();
+    };
+
+
     render() {
         return (
         <div className="billList">
             <div className='header'>
-                <form>
-                    <input placeholder='enter a bill'></input>
+                <form onSubmit={this.addBill}>
+                    <input ref={ (a) => this._inputElement = a} placeholder='enter a bill'>
+                    </input>
                     <button type='submit'>Add</button>
                 </form>
             </div>
